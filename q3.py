232ea00532a6a95115a514ef3a2ad8d67cc3e793
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2026-09-23 13:57:26 +0530

    Refactor factorial calculation to use user input
    Updated the factorial computation to accept user input and print the result.

--- q3.py
+++ q3.py
@@ -1,4 +1,11 @@
-n=8
+'''
+Write a program which can compute the factorial of a given numbers.The
+results should be printed in a comma-separated sequence on a single
+line.Suppose the following input is supplied to the program:8
+Then, the output should be:40320
+'''
+
+n=input()
 fact =1
 for i in range (1,n+1):
     fact *= i
